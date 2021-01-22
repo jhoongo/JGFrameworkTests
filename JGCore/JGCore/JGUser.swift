467d4f2a1d0c3e7b404b3632de42fa30696a7b07
James Go <jhoongo@gmail.com>
2021-01-21 23:10:48 -0800

    SPM 0.1.5

--- JGCore/JGCore/JGUser.swift
+++ JGCore/JGCore/JGUser.swift
@@ -12,11 +12,11 @@ open class JGUser {
     var age: Int
     
     public init() {
-        self.name = "JGCore Name"
+        self.name = "JGCore Name 1"
         self.age = 1
     }
     
     open func getName() -> String {
-        return self.name
+        return self.name + " updated"
     }
 }
